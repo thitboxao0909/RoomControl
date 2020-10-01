11466594e699dab42415a60b00a7f36ce6524915
Hoang Do <20427568@students.ltu.edu.au>
2020-10-01 16:43:08 +1000

    AddProperty method

--- app/src/main/java/com/example/auth/LandlordMain.java
+++ app/src/main/java/com/example/auth/LandlordMain.java
@@ -6,14 +6,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class LandlordMain extends AppCompatActivity {
 
-    private Button settingBtn;
+    private Button settingBtn, addProperty;
     private TextView Name;
+    private FirebaseDatabase rootNode;
+    private LinearLayout propertyList;
+
+    DatabaseReference reference;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,6 +38,16 @@ public class LandlordMain extends AppCompatActivity {
         Name = (TextView) findViewById(R.id.landLordName);
         Name.setText(firebaseAuth.getCurrentUser().getDisplayName());
 
+        rootNode = FirebaseDatabase.getInstance();
+        addProperty = (Button) findViewById(R.id.landLordAddPropertyBtn);
+        addProperty.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LandlordMain.this, AddProperty.class));
+            }
+        });
+
+        propertyList = (LinearLayout) findViewById(R.id.landlordScroll);
 
     }
 }
